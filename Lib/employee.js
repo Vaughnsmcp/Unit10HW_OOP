6c77c9410d407fea883fd7efde709ef34e253adf
Vaughn McPherson <Vaughnsmcp@gmail.com>
2020-02-05 16:49:04 -0800

    initial code

---
+++ Lib/employee.js
@@ -0,0 +1,18 @@
+class Employee {
+    constructor(id, name, title) {
+        this.id = id;
+        this.name = name;
+        this.title = title;
+    }
+}
+module.exports = Employee;
+
+
+
+
+
+
+
+
+
+
